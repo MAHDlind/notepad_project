ec1bc14c6ac1895cf05605c1ed082836d3a5ab32
Mahdi Hoseinkhani <qwrwfwtsewqr@gmail.com>
2025-08-02 08:11:16 +0330

    adding templates to urls.py, created views for each template
    Signed-off-by: Mahdi Hoseinkhani <qwrwfwtsewqr@gmail.com>

--- notes/urls.py
+++ notes/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from notes.views import home_page
+from notes.views import home_page, login_page, register_page, note_page
 
 urlpatterns = [
-    path('', home_page, name='home')
+    path('', home_page, name='home'),
+    path('notes_form', note_page, name='add_note'),
+    path('loagin', login_page, name='login'),
+    path('register', register_page, name='register'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -3,4 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def home_page(request):
-    return render(request, 'index.html')
+    return render(request, 'notes/index.html')
+
+def login_page(request):
+    return render(request, 'notes/login.html')
+
+def register_page(request):
+    return render(request, 'notes/register.html')
+
+def note_page(request):
+    return render(request, 'notes/note_form.html')
